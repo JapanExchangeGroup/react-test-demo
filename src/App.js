e5af2939961531b48a54c6dae9f61fde7d70e06a
Nicolas Modrzyk <hellonico@gmail.com>
2022-03-07 14:01:48 +0900

    some test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Link from './Link.js';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Link page="http://www.lemonde.fr">LeMonde</Link>
       </header>
     </div>
   );
